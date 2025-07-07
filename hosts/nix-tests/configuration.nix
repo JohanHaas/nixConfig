259fc5a0546ac635ec9831185b195951b58c08b0
JohanHaas <johanHaas@gmail.com>
2025-07-07 23:48:07 +0200

    nvidia drivers

--- hosts/nix-tests/configuration.nix
+++ hosts/nix-tests/configuration.nix
@@ -20,6 +20,21 @@
 
   system.stateVersion = "25.05";
 
-  virtualisation.virtualbox.guest.enable = true;
+    hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;  # Wichtig für Laptops/Energiesparmodus
+    open = false;  # Falls du den Open-Source-Treiber testen willst (nicht empfohlen für Gaming)
+    nvidiaSettings = true;  # Für nvidia-settings GUI
+    package = config.boot.kernelPackages.nvidiaPackages.stable;  # Stabile Version
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    # NVIDIA-GPU als Haupt-GPU:
+    nvidiaBusId = "PCI:1:0:0";  # Anpassen mit `lspci | grep -i nvidia`
+    # Intel/AMD-GPU:
+    intelBusId = "PCI:0:2:0";   # Anpassen mit `lspci | grep -i vga`
+  };
 
 }
