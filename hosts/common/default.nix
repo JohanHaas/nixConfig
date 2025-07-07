896c233bad663263e0736ee611c18cbfde565878
JohanHaas <johanHaas@gmail.com>
2025-07-07 19:28:28 +0200

    common

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,5 +1,47 @@
+{ config, lib, pkgs, modulesPath, ... }:
 {
-    imports = [
+  imports = [
         
-    ];
+  ];
+
+  environment.systemPackages = with pkgs; [
+    git
+    neofetch
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  console.keyMap = "de";
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+    randomizedDelaySec = "30min";
+    persistent = true;
+  };
+
 }
