2c648fe6569b061e0373182a64cfa24e7c0fbd27
JohanHaas <johanHaas@gmail.com>
2025-07-08 15:29:50 +0200

    vm drivers

--- hosts/nix-vm/configuration.nix
+++ hosts/nix-vm/configuration.nix
@@ -22,4 +22,8 @@
 
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
+  services.xserver.videoDrivers = [ "virtualbox" ];
+  services.xserver.enable = true;
 }
