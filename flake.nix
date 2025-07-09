e884b6c4ebe9023993b523fe5f24dfe61659ea77
JohanHaas <johanHaas@gmail.com>
2025-07-09 11:15:19 +0200

    stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 
   inputs = {
 
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos";
 
     home-manager = {
       url = "github:nix-community/home-manager";
