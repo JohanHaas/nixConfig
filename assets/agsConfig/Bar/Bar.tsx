370db66532ebfa52626bd165da582bec49cb27b4
JohanHaas <johanHaas@gmail.com>
2025-07-12 18:37:49 +0200

    testing

--- assets/agsConfig/Bar/Bar.tsx
+++ assets/agsConfig/Bar/Bar.tsx
@@ -20,6 +20,7 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
             exclusivity={Astal.Exclusivity.EXCLUSIVE}
             anchor={TOP | LEFT | RIGHT}
             application={app}
+            layer={Astal.Layer.TOP}
         >
             <centerbox hexpand={true}>
                 <box $type="center">
