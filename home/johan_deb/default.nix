250301aac5a4e03cd43b767e5e61fe8c6a91dc32
JohanHaas <johanhaas99@gmail.com>
2025-12-02 09:15:14 +0100

    deb

--- home/johan_deb/default.nix
+++ home/johan_deb/default.nix
@@ -12,10 +12,8 @@
   home.stateVersion = "25.05";
 
   home.packages = with pkgs; [
-<<<<<<< HEAD:home/johan_mint/default.nix
-=======
     papirus-icon-theme
->>>>>>> 198cd34 (deb):home/johan_deb/default.nix
+    discord
   ];
 
   home.file = {
@@ -26,12 +24,9 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-<<<<<<< HEAD:home/johan_mint/default.nix
-=======
 
   nix = {
     package = pkgs.nix;
     settings.experimental-features = ["nix-command" "flakes"];
   };
->>>>>>> 198cd34 (deb):home/johan_deb/default.nix
 }
